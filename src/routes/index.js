4fa3c8f54e543e361ef0e3aba4d9db7920485493
mdragic04 <53371014+mdragic04@users.noreply.github.com>
2022-02-05 14:51:01 +0100

    Added initial router

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+
+/*const loginRouter = require("login-router");
+const registrationRouter = require("registration-router");
+const ticketsRouter = require("tickets-router");
+const usersRouter = require("users-router");
+
+router.use("/login", loginRouter);
+router.use("/registration", registrationRouter);
+
+router.use("/tickets", ticketsRouter);
+
+router.use("/users", usersRouter);*/
+
+module.exports = router;

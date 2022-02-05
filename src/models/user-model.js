dbfc45e7dc0bc8856a0a517131e972dd39451950
mdragic04 <53371014+mdragic04@users.noreply.github.com>
2022-02-05 16:49:45 +0100

    Added timestamp

--- src/models/user-model.js
+++ src/models/user-model.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
   name: String,
   email: String,
   accessToken: String,
+  created: { type: Number, default: Date.now },
 });
 
 module.exports = mongoose.model("User", userSchema);

e9dc917f1f7daaaa37607b416323fb84f5c81f20
mdragic04 <53371014+mdragic04@users.noreply.github.com>
2022-02-06 13:23:55 +0100

    added sort and pagination support

--- src/services/ticket-services.js
+++ src/services/ticket-services.js
@@ -4,8 +4,8 @@ const getTicket = ({ query, projection }) => {
   return Ticket.findOne(query, projection);
 };
 
-const getTickets = ({ query, projection }) => {
-  return Ticket.find(query, projection);
+const getTickets = ({ query, projection, sort, skip, limit }) => {
+  return Ticket.find(query, projection).sort(sort).skip(skip).limit(limit);
 };
 
 const updateTicket = (ticket) => {
